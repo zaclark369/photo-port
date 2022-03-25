b5be5dbc507366aa55d9720ceda0d31c0fb56ae4
Zachary Clark <zacclark369@gmail.com>
2022-03-24 18:52:04 -0700

    added state to input form for contact

---
+++ src/components/Contact/index.js
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+
+function ContactForm() {
+  const [formState, setFormState] = useState({ name: '', email: '', message: '' });
+  const { name, email, message } = formState;
+  // JSX
+function handleChange(e) {
+  setFormState({ ...formState, [e.target.name]: e.target.value})
+}
+
+function handleSubmit(e) {
+  e.preventDefault();
+  console.log(formState);
+}
+
+  return (
+    <section>
+      <h1>Contact Message</h1>
+      <form id="contact-form" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name:</label>
+          <input type="text" defaultValue={name} onChange={handleChange} name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email address:</label>
+          <input type="email" defaultValue={email} name="email" onChange={handleChange} />
+        </div>
+        <div>
+          <label htmlFor="message">Message:</label>
+          <textarea name="message" defaultValue={message} onChange={handleChange} rows="5" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default ContactForm;
